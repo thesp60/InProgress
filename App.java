9f7c5abc365c0ef36b26fa23bcec14f76294c9e0
Katacoda Scenario <scenario@katacoda.com>
2021-10-01 15:04:11 +0000

    MQ broker stub

---
+++ App.java
@@ -0,0 +1,18 @@
+//package org.apache.activemq.simple;
+
+import org.apache.activemq.broker.*;
+//import org.junit.jupiter.*;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+      // Prints "Hello, World" to the terminal window.
+      System.out.println("Hello, World");
+
+      //try{
+        BrokerService broker = BrokerFactory.createBroker("Test");
+      //} catch( Exception e){
+
+      //}
+      
+    }
+}
