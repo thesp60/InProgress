b9ce52cf3a102e8a78743a2f496f5d3d15e4b791
Katacoda Scenario <scenario@katacoda.com>
2021-10-01 15:27:58 +0000

    configurable

--- Server.java
+++ Server.java
@@ -2,6 +2,7 @@ package biz.burse;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.*;
 import java.io.*;
@@ -17,9 +18,13 @@ public class Server {
     public static void main(final String[] args) throws Exception {
 
         // Initiate source connection - shelter
-        int port = 0;
+        //int port = 0;
+        final InetAddress shelterAddress; 
         try (BufferedReader reader = new BufferedReader(new FileReader("sharedInfo.cfg"))) {
-            port = Integer.valueOf(reader.readLine().split(" ")[0]);
+            final String[] hostPort = reader.readLine().split(" ")[0];
+            final String host = hostPort[0].trim();
+            port = Integer.valueOf(hostPort[1]);
+            shelterAddress = new InetSocketAddress(host, port);
 
         } catch (final FileNotFoundException e) {
             System.out.println("no file" + e);
@@ -27,7 +32,7 @@ public class Server {
             System.out.println("system issue" + e);
         }
 
-        try (Socket connection = new Socket(InetAddress.getByName("localhost"), port)) {
+        try (Socket connection = new Socket(shelterAddress) ){
 
         } catch (final IOException e) {
 
