bf74f16ee89fbad824120037341501ab81318c13
Katacoda Scenario <scenario@katacoda.com>
2021-10-01 15:20:27 +0000

    Initial with imports

--- Server.java
+++ Server.java
@@ -1,30 +1,35 @@
 package biz.burse;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.*;
+import java.io.*;
+import java.util.Scanner;
+import javax.net.*;
+import java.net.ServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
 
 import org.apache.activemq.broker.*;
 
 public class Server {
-    public static void main(String[] args) throws Exception{
+    public static void main(final String[] args) throws Exception {
 
-        //Initiate source connection - shelter
-        int port=0;
-        try( BufferedReader reader = new BufferedReader(new FileReader("sharedInfo.cfg"))){
-             port = Integer.valueOf(reader.readLine().split(" ")[0]);
-            
+        // Initiate source connection - shelter
+        int port = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("sharedInfo.cfg"))) {
+            port = Integer.valueOf(reader.readLine().split(" ")[0]);
 
-        } 
-        catch (FileNotFoundException e){
-            System.out.println("no file" +e);
-        }catch (IOException e){
-            System.out.println("system issue" +e);
+        } catch (final FileNotFoundException e) {
+            System.out.println("no file" + e);
+        } catch (final IOException e) {
+            System.out.println("system issue" + e);
         }
-        
 
-        try( Socket connection = new Socket(InetAddress.getByName("localhost"),port)){
- 
+        try (Socket connection = new Socket(InetAddress.getByName("localhost"), port)) {
 
-        } catch (IOException e){
+        } catch (final IOException e) {
 
         }
 
